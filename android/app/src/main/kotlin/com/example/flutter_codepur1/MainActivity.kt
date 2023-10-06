77f8461dfeb410fe18f2649c56067ca51229dcea
Ankush Koundal <ankush88385@gmail.com>
2023-10-07 00:24:39 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/flutter_codepur1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_codepur1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_codepur1/views/home_view.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "FlutterRun",
+      home: HomeView() 
+      );
+  }
+}
