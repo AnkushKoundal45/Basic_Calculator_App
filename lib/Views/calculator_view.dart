8d7f6f32d7ea9f2d0b4df82d652fa3f188fc1d25
Ankush Koundal <ankush88385@gmail.com>
2023-10-07 11:23:30 +0530

    added calculator widget

---
+++ lib/Views/calculator_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class calculator extends StatelessWidget {
+  const calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
